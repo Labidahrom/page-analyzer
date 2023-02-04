8e74d45e21fac1317b476c745f1520df31d06170
Labidahrom <labidahrom@yandex.ru>
2023-02-04 10:25:37 +0600

    fix parser

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -32,8 +32,10 @@ def add_to_url_table(site_url):
 
 def make_url_check(url):
     session = requests.session()
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0",
-               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) "
+                             "Gecko/20100101 Firefox/60.0",
+               "Accept": "text/html,application/xhtml+xml,"
+                         "application/xml;q=0.9,*/*;q=0.8"}
     page = session.get(url, headers=headers)
     status_code = page.status_code
     print(status_code)
