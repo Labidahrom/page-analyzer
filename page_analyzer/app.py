f1fc4beecc2d139424a4c0797032530ae4a6d173
Labidahrom <labidahrom@yandex.ru>
2023-02-11 18:49:42 +0600

    finish 1 check

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -9,7 +9,6 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
@@ -17,29 +16,16 @@ app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
 
-def get_database_entry(query, *args):
+def connect_to_database():
     conn = psycopg2.connect(DATABASE_URL)
-    cursor = conn.cursor()
-    cursor.execute(query, (args))
-    entry = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return entry
-
-
-def make_database_entry(query, *args):
-    conn = psycopg2.connect(DATABASE_URL)
-    cursor = conn.cursor()
-    cursor.execute(query, (args))
-    conn.commit()
-    conn.close()
+    return conn.cursor(), conn
 
 
 def get_date():
     return datetime.now().strftime("%Y-%m-%d")
 
 
-def make_url_check(url):
+def prepare_seo_data(url):
     session = requests.session()
     headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) "
                              "Gecko/20100101 Firefox/60.0",
@@ -49,24 +35,29 @@ def make_url_check(url):
     page.raise_for_status()
     status_code = page.status_code
     soup = BeautifulSoup(page.content, "html.parser")
-    title = soup.find('title')
-    h1 = soup.find('h1')
-    description = soup.find("meta", {"name": "description"})
-    if title:
-        title = title.get_text()
-    if h1:
-        h1 = h1.get_text()
-    if description:
-        description = description.attrs.get("content", "")
-    return status_code, title, h1, description
+    tags = [('title', {}), ('h1', {}), ('meta', {"name": "description"})]
+    tags_text = {'title': '', 'h1': '', 'meta': ''}
+    for tag, conditions in tags:
+        node = soup.find(tag, conditions)
+        if node:
+            if tag == 'meta':
+                tags_text[tag] = node.attrs.get("content", "")
+            else:
+                tags_text[tag] = node.get_text()
+    return status_code, tags_text['title'], tags_text['h1'], \
+        tags_text['meta']
 
 
 def add_to_url_checks_table(id, status_code, title, h1, description):
     date = get_date()
-    make_database_entry("INSERT INTO url_checks (url_id, status_code, title, "
-                        "h1, description, created_at) VALUES (%s, %s, %s, %s,"
-                        " %s, %s)", id, status_code, title, h1, description,
-                        date)
+    cursor, connection = connect_to_database()
+    cursor.execute("INSERT INTO url_checks (url_id, status_code, title, "
+                   "h1, description, created_at) VALUES (%s, %s, %s, %s,"
+                   " %s, %s)", (id, status_code, title, h1, description,
+                                date))
+    connection.commit()
+    cursor.close()
+    connection.close()
 
 
 @app.route('/')
@@ -79,11 +70,15 @@ def get_index():
 
 @app.route('/urls')
 def get_urls():
-    site_list = get_database_entry("SELECT DISTINCT ON (id) * FROM urls LEFT"
-                                   " JOIN (SELECT url_id, status_code,"
-                                   " created_at AS last_check_date FROM"
-                                   " url_checks ORDER BY id DESC) AS checks ON"
-                                   " urls.id = checks.url_id ORDER BY id DESC;")
+    cursor, connection = connect_to_database()
+    cursor.execute("SELECT DISTINCT ON (id) * FROM urls LEFT"
+                   " JOIN (SELECT url_id, status_code,"
+                   " created_at AS last_check_date FROM"
+                   " url_checks ORDER BY id DESC) AS checks ON"
+                   " urls.id = checks.url_id ORDER BY id DESC;")
+    site_list = cursor.fetchall()
+    cursor.close()
+    connection.close()
     return render_template(
         'site_list.html',
         site_list=site_list
@@ -92,6 +87,7 @@ def get_urls():
 
 @app.post('/urls')
 def post_urls():
+    cursor, connection = connect_to_database()
     parse_url = urlparse(request.form.to_dict()['url'])
     site_url = f'{parse_url.scheme}://{parse_url.hostname}'
     if not url(site_url) or len(site_url) > 255:
@@ -100,39 +96,50 @@ def post_urls():
         return render_template(
             'index.html', messages=messages
         ), 422
-    entry = get_database_entry('SELECT * FROM urls WHERE name = %s', site_url)
+    cursor.execute('SELECT * FROM urls WHERE name = %s', (site_url,))
+    entry = cursor.fetchall()
     if entry:
         flash('Страница уже существует', 'info')
         return redirect(url_for('get_url', id=entry[0][0]))
     date = get_date()
-    make_database_entry("INSERT INTO urls (name, created_at) VALUES (%s, %s)",
-                        site_url, date)
-    [(id,)] = get_database_entry('SELECT id FROM urls WHERE name = %s',
-                                 site_url)
+    cursor.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)",
+                   (site_url, date))
+    connection.commit()
+    cursor.execute('SELECT id FROM urls WHERE name = %s', (site_url,))
+    [(id,)] = cursor.fetchall()
+    cursor.close()
+    connection.close()
     flash('Страница успешно добавлена', 'success')
     return redirect(url_for('get_url', id=id))
 
 
 @app.post('/urls/<id>/checks')
 def post_url_check(id):
-    [(id, url, date)] = get_database_entry('SELECT * FROM urls WHERE id = %s',
-                                           id)
+    cursor, connection = connect_to_database()
+    cursor.execute('SELECT * FROM urls WHERE id = %s', (id,))
+    [(id, url, date)] = cursor.fetchall()
     try:
-        status_code, title, h1, description = make_url_check(url)
+        status_code, title, h1, description = prepare_seo_data(url)
     except requests.exceptions.RequestException:
         flash('Произошла ошибка при проверке', 'danger')
         return redirect(url_for('get_url', id=id))
     add_to_url_checks_table(id, status_code, title, h1, description)
+    cursor.close()
+    connection.close()
     flash('Страница успешно проверена', 'success')
     return redirect(url_for('get_url', id=id))
 
 
 @app.route('/urls/<id>')
 def get_url(id):
-    [(id, name, date)] = get_database_entry('SELECT * FROM urls WHERE id = %s',
-                                            id)
-    site_checks = get_database_entry('SELECT * FROM url_checks WHERE url_id = '
-                                     '%s ORDER BY id DESC', id)
+    cursor, connection = connect_to_database()
+    cursor.execute('SELECT * FROM urls WHERE id = %s', (id,))
+    [(id, name, date)] = cursor.fetchall()
+    cursor.execute('SELECT * FROM url_checks WHERE url_id = %s '
+                   'ORDER BY id DESC', (id,))
+    site_checks = cursor.fetchall()
+    cursor.close()
+    connection.close()
     messages = get_flashed_messages(with_categories=True)
     return render_template(
         'single_site.html',
